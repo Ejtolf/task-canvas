fe2c467cb9623f770885a9b46564cd9253b87229
ProgasiFox <96259001+ProgasiFox@users.noreply.github.com>
2023-11-21 08:48:09 +0300

    Test version of <<clear tasks>> is done and fixed some bugs.

--- src/Components/InformationBlock.tsx
+++ src/Components/InformationBlock.tsx
@@ -1,19 +1,23 @@
 import React, { useEffect, useState } from "react";
 import "../Styles/InformationBlock.css";
-import { Button } from "@mui/material";
+import { Button, Slide, SlideProps, Snackbar, Typography } from "@mui/material";
+import { TransitionProps } from "@mui/material/transitions";
 
 export default function InformationBlock() {
     const [numberOfTasksForToday, setNumberOfTasksForToday] = useState<number>(0);
     const [numberOfTasksForTomorrow, setNumberOfTasksForTomorrow] = useState<number>(0);
     const [lastTaskTime, setLastTaskTime] = useState<string | number | Date>(0);
-    const [currentTime, setCurrentTime] = useState(new Date());
     const [tasksRemains, setTasksRemains] = useState<string | number>(0);
 
     // DATE
+    const [currentTime, setCurrentTime] = useState(new Date());
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     const today = new Date();
     const currentDay = today.getDate();
     const currentMonth = months[today.getMonth()];
+
+    // TYPES
+    // type TransitionProps = Omit<SlideProps, "direction">;
 
     // CLOCK
     useEffect(() => {
@@ -24,9 +28,25 @@ export default function InformationBlock() {
         return () => clearInterval(interval);
     }, []);
 
+    // const TransitionDown = (props: TransitionProps) => {
+    //     return <Slide {...props} direction="down" />;
+    // }
+
     const handleAddTask = () => {
         setNumberOfTasksForToday(numberOfTasksForToday + 1);
         setLastTaskTime(currentTime.toLocaleTimeString());
+
+        // const [transition, setTransition] = React.useState<React.ComponentType<TransitionProps> | undefined>(undefined);
+
+        // setTransition(() => Transition);
+    }
+
+    const handleDeleteAllTasks = () => { //TODO: This is test function. Then it must be changed to be useful.
+        setNumberOfTasksForToday(0);
+        setNumberOfTasksForTomorrow(0);
+        setLastTaskTime(0);
+        setTasksRemains(0);
+
     }
 
     return (
@@ -55,9 +75,8 @@ export default function InformationBlock() {
                 </div>
                 {/* ---------------------------------------------------------------- */}
                 <div className="manager-buttons">
-                    <Button variant="contained" onClick={handleAddTask} style={{
-                    }}>Add New Task</Button>
-                    <Button variant="contained">Clear completed tasks</Button>
+                    <Button variant="contained" onClick={handleAddTask}>Add New Task</Button>
+                    <Button variant="contained" onClick={handleDeleteAllTasks}>Clear completed tasks</Button>
                 </div>
             </div>
         </div>
